56ecb9de46cb225410073e80f5fcdce375b5305c
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-04 07:00:22 +0600

    Update pool.php

--- pool.php
+++ pool.php
@@ -6,16 +6,7 @@
   <title>  </title>
 </head>
 <body>
-     <!DOCTYPE html>
-<html>
-<head>
-  <meta charset='UTF-8'>
-  <meta name= 'viewport' content='width=device-width, initial-scale=1.0'>
-  <title>  </title>
-</head>
-<body>
 
-</body>
 </html>
 </body>
 </html>
